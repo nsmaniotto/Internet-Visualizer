a6eca85c19bbdf7ff21924cc22404ee043b3b5d2
nsmaniotto <smaniotto.nathan@gmail.com>
2020-10-20 22:09:39 +0200

    add host shortcut behaviour completed, needs component Host

--- src/js/shortcuts.js
+++ src/js/shortcuts.js
@@ -13,6 +13,19 @@ shortcutAddSwitch.click(function() { addSwitch() });
 
 function addHost() {
 	console.log("new host");
+	// Ask for the host name
+	var hostName = null;
+
+	while(hostName == null || hostName == "")
+	{
+		hostName = prompt("Please name the new host", "unnamed_host");
+	}
+
+	// Create a new host instance with the name
+
+
+	// Add the new host instance to the sandbox
+
 }
 
 function addHub() {
