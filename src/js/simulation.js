2fe49f856397a12c7c0ee39125eed59fcc65ca5e
nsmaniotto <smaniotto.nathan@gmail.com>
2020-11-09 20:29:00 +0100

    dynamically generated simulation control buttons

--- src/js/simulation.js
+++ src/js/simulation.js
@@ -2,6 +2,7 @@ class Simulation {
 	constructor() {
 		if(! Simulation.instance){
 			Simulation.instance = this;
+			this.generateHTML();
 		}
 
 		return Simulation.instance;
@@ -13,6 +14,33 @@ class Simulation {
 		datas.show();
 	}
 
+	generateHTML() {
+		var simulationControls = $('#simulationControls');
+
+		var previousButton = $('<button/>', {
+			'id': 'simulationControlPrevious',
+			'type': 'button',
+	        'class': 'btn btn-secondary'
+	    });
+	    previousButton.html('Previous');
+	    simulationControls.append(previousButton);
+
+	    var playPauseButton = $('<button/>', {
+			'id': 'simulationControlPlayPause',
+			'type': 'button',
+	        'class': 'btn btn-secondary'
+	    });
+	    playPauseButton.html('Play');
+	    simulationControls.append(playPauseButton);
+
+	    var nextButton = $('<button/>', {
+			'id': 'simulationControlNext',
+			'type': 'button',
+	        'class': 'btn btn-secondary'
+	    });
+	    nextButton.html('Next');
+	    simulationControls.append(nextButton);
+	}
 }
 
 var simulation = new Simulation();
