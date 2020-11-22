28ad9acde93d064b65cfabaa5e134c5fc1dd4ef3
nsmaniotto <smaniotto.nathan@gmail.com>
2020-11-22 16:24:49 +0100

    reorganized main

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,47 @@
+/* VARIABLES */
+
+// Cache
 var componentsList = new Array();
 
+// Counters
 var componentsCounter = 0;
-
-var dataInformationsDiv = $('#dataInformations');
-var dataDiv = $('#data');
-var simulationControlsDiv = $('#simulationControls');
-
-
-var dataSender = new DataSender('#sandbox');
-
-var interfacesCounter = 0;
-var currentlyLinkingInterfaces = false;
-var sourceInterface = null;
-var destinationInterface = null;
-
 var layersCounter = 0;
-
 var protocolsCounter = 0;
+var interfacesCounter = 0;
+
+// Components connection
+	// Flags
+	var currentlyLinkingInterfaces = false;
+
+	// Cache
+	var sourceInterface = null;
+	var destinationInterface = null;
+
+// GUI
+	// Shortcuts
+	var shortcutAddHost = $( "#shortcutAddHost" );
+	var shortcutAddHub = $( "#shortcutAddHub" );
+	var shortcutAddSwitch = $( "#shortcutAddSwitch" );
+	var shortcutAddRouter = $( "#shortcutAddRouter" );
+
+	// Sandbox
+	var sandboxDiv = $( "#sandbox" );
+
+	// Data sender
+	var dataSender = new DataSender('#sandbox');
+
+	// Data visualizer
+	var dataInformationsDiv = $('#dataInformations');
+	var dataDiv = $('#data');
+	var simulationControlsDiv = $('#simulationControls');
 
 
+/* CALLS */
 
 window.onresize = sandboxResize;
 
-sandboxResize();
+createProtocolShortcuts();
 
 var simulation = new Simulation();
 
-var sandboxDiv = $( "#sandbox" );
-
-var shortcutAddHost = $( "#shortcutAddHost" );
-var shortcutAddHub = $( "#shortcutAddHub" );
-var shortcutAddSwitch = $( "#shortcutAddSwitch" );
-var shortcutAddRouter = $( "#shortcutAddRouter" );
-
-createProtocolShortcuts();
+sandboxResize();
